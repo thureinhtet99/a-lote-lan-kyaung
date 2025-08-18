307f108388b2ac5561786284f74ad7bf984353b1
Thureinhtet99 <thureinhtet653@gmail.com>
2025-08-18 17:00:59 +0700

    chore: update dependencies and add new packages for UI components and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebarClient } from "./_AppSideBarClient";
 
 export default function Home() {
   return (
-    <div>
-      <main>thi is main</main>
-      <footer>this is footer</footer>
-    </div>
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        {/* Left */}
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">A lote lan kyaung</span>
+          </SidebarHeader>
+          <SidebarContent>text here....</SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>button</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+
+        {/* Right */}
+        <main className="flex-1">text</main>
+      </AppSidebarClient>
+    </SidebarProvider>
   );
 }
