1a9ce9a40c116448b6b532f79922e98401adf7a5
thureinhtet99 <ignite25399@gmail.com>
2026-03-13 14:53:36 +0700

    refactor: update database connection to use Neon and adjust environment variables in .env.example

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,8 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "@/drizzle/schema";
 
-// Create a PostgreSQL pool
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
-});
+const sql = neon(process.env.DATABASE_URL!);
 
-// Create a drizzle client with query
-export const db = drizzle(pool, { schema });
+export const db = drizzle(sql, { schema });
